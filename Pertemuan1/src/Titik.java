eb22739bbf8906b200313e59d0fd65f21fcd338a
Nexvaulus <moehammad.fikr@gmail.com>
2026-02-19 19:02:51 +0700

    Pertemuan 1

---
+++ Pertemuan1/src/Titik.java
@@ -0,0 +1,49 @@
+/* Nama File    : Titik.java
+ * Deskripsi    : berisi atribut dan method dalam clans Titik
+ * Pembuat      :
+ * Tanggal      :
+ */
+
+
+public class Titik {
+    /************ATRIBUT************/
+    double absis;
+    double ordinat;
+
+    /************METHOD************/
+    Titik() {
+        absis = 0;
+        ordinat = 0;
+    }
+
+    // mengembalikan nilai absis
+    double getAbsis() {
+        return absis;
+    }
+    
+    // mengembalikan nilai ordinat
+
+    double getOrdinat() {
+        return ordinat;
+    }
+
+    // mengeset absis titik dengan nilai baru x
+    void setAbsis(double x) {
+        absis = x;
+    }
+
+    void setOrdinat(double y) {
+        ordinat = y;
+    }
+
+    // menggeser nilai absis dan ordinat titik masing masing sejauh x dan y
+    void geser(double x, double y) {
+        absis = absis + x;
+        ordinat = ordinat + y;
+    }
+    
+    // mencetak koordinat titik
+    void printTitik() {
+        System.out.println("Titik (" + absis + "," + ordinat + ")");
+    }
+}
